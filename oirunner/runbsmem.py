9ea4b98c03afa3a9509aa82138160496453ebdea
John Young <jsy1001@cam.ac.uk>
2018-09-06 18:11:01 +0100

    Add modeltype & modelwidth args

--- oirunner/runbsmem.py
+++ oirunner/runbsmem.py
@@ -6,13 +6,18 @@ import os.path
 BSMEM = 'bsmem'
 
 DEFAULT_DIM = 128
+DEFAULT_MT = 3
+DEFAULT_MW = 10.0
 
 
-def reconst_grey(datafile, outputfile, dim, pixelsize):
+def reconst_grey(datafile, outputfile, dim, pixelsize,
+                 modeltype, modelwidth):
     args = [BSMEM, '--noui',
             '--data=%s' % datafile,
             '--clobber', '--output=%s' % outputfile,
-            '--dim=%d' % dim]
+            '--dim=%d' % dim,
+            '--mt=%d' % modeltype,
+            '--mw=%f' % modelwidth]
     if pixelsize is not None:
         args += ['--pixelsize=%f' % pixelsize]
     print("Running '%s'" % ' '.join(args))
@@ -21,17 +26,21 @@ def reconst_grey(datafile, outputfile, dim, pixelsize):
     print('\n%s %s' % (outputfile, result))
 
 
-def run_grey_basic(datafile, dim=DEFAULT_DIM, pixelsize=None):
+def run_grey_basic(datafile, dim=DEFAULT_DIM, pixelsize=None,
+                   modeltype=DEFAULT_MT, modelwidth=DEFAULT_MW):
     stem = os.path.splitext(os.path.basename(datafile))[0]
     outputfile = 'bsmem_%s.fits' % stem  # in CWD
-    reconst_grey(datafile, outputfile, dim, pixelsize)
+    reconst_grey(datafile, outputfile, dim, pixelsize,
+                 modeltype, modelwidth)
 
 
 def run_grey_2step(datafile, dim=DEFAULT_DIM, pixelsize=None,
+                   modeltype=DEFAULT_MT, modelwidth=DEFAULT_MW,
                    basmax=25.0):
     pass
 
 
 def run_grey_nstep(datafile, dim=DEFAULT_DIM, pixelsize=None,
+                   modeltype=DEFAULT_MT, modelwidth=DEFAULT_MW,
                    basmax=[25.0, 30.0, 40.0, 50.0]):
     pass
