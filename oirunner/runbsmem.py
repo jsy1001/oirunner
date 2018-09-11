906d39b1decd585ed89d21a207ebf62d0ac71860
John Young <jsy1001@cam.ac.uk>
2018-09-11 18:16:46 +0100

    Implement run_grey_2step()

--- oirunner/runbsmem.py
+++ oirunner/runbsmem.py
@@ -1,7 +1,10 @@
-from subprocess import run, PIPE, CalledProcessError
-# from astropy.io import fits
 import os.path
-# import numpy as np
+from subprocess import run, PIPE, CalledProcessError
+
+from astropy.io import fits
+
+from .priorimage import makesf
+
 
 BSMEM = 'bsmem'
 
@@ -10,16 +13,13 @@ DEFAULT_MT = 3
 DEFAULT_MW = 10.0
 
 
-def reconst_grey(datafile, outputfile, dim, pixelsize,
-                 modeltype, modelwidth):
-    args = [BSMEM, '--noui',
-            '--data=%s' % datafile,
-            '--clobber', '--output=%s' % outputfile,
-            '--dim=%d' % dim,
-            '--mt=%d' % modeltype,
-            '--mw=%f' % modelwidth]
-    if pixelsize is not None:
-        args += ['--pixelsize=%f' % pixelsize]
+def get_outputfile(datafile, iteration):
+    dirname, basename = os.path.split(datafile)
+    stem = os.path.splitext(basename)[0]
+    return os.path.join(dirname, 'bsmem_%d_%s.fits' % (iteration, stem))
+
+
+def run_bsmem(args, outputfile):
     print("Running '%s'" % ' '.join(args))
     try:
         process = run(args, check=True, stdout=PIPE, stderr=PIPE)
@@ -33,22 +33,52 @@ def reconst_grey(datafile, outputfile, dim, pixelsize,
         print("FAILED: %s" % e.stderr.decode('utf-8'))
 
 
+def reconst_using_model(datafile, outputfile, dim, modeltype, modelwidth,
+                        pixelsize=None, uvmax=None):
+    args = [BSMEM, '--noui',
+            '--data=%s' % datafile,
+            '--clobber', '--output=%s' % outputfile,
+            '--dim=%d' % dim,
+            '--mt=%d' % modeltype,
+            '--mw=%f' % modelwidth]
+    if pixelsize is not None:
+        args += ['--pixelsize=%f' % pixelsize]
+    if uvmax is not None:
+        args += ['--uvmax=%f' % uvmax]
+    run_bsmem(args, outputfile)
+
+
+def reconst_using_image(datafile, outputfile, dim, pixelsize, imagehdu,
+                        uvmax=None):
+    imagefile = 'tempsf.fits'
+    imagehdu.writeto(imagefile, overwrite=True)
+    args = [BSMEM, '--noui',
+            '--data=%s' % datafile,
+            '--clobber', '--output=%s' % outputfile,
+            '--dim=%d' % dim,
+            '--pixelsize=%f' % pixelsize,
+            '--sf=%s' % imagefile]
+    if uvmax is not None:
+        args += ['--uvmax=%f' % uvmax]
+    run_bsmem(args, outputfile)
+
+
 def run_grey_basic(datafile, dim=DEFAULT_DIM, pixelsize=None,
                    modeltype=DEFAULT_MT, modelwidth=DEFAULT_MW):
-    dirname, basename = os.path.split(datafile)
-    stem = os.path.splitext(basename)[0]
-    outputfile = os.path.join(dirname, 'bsmem_%s.fits' % stem)
-    reconst_grey(datafile, outputfile, dim, pixelsize,
-                 modeltype, modelwidth)
+    reconst_using_model(datafile, get_outputfile(datafile, 1), dim,
+                        modeltype, modelwidth, pixelsize=pixelsize)
 
 
 def run_grey_2step(datafile, dim=DEFAULT_DIM, pixelsize=None,
                    modeltype=DEFAULT_MT, modelwidth=DEFAULT_MW,
-                   basmax=25.0):
-    pass
-
-
-def run_grey_nstep(datafile, dim=DEFAULT_DIM, pixelsize=None,
-                   modeltype=DEFAULT_MT, modelwidth=DEFAULT_MW,
-                   basmax=[25.0, 30.0, 40.0, 50.0]):
-    pass
+                   uvmax1=2.0e6, fwhm=2.0, threshold=0.05):
+    # :TODO: choose automatic pixelsize outside of BSMEM?
+    #        or run 1 iteration on full dataset?
+    # :TODO: intelligent defaults for uvmax1, fwhm?
+    out1file = get_outputfile(datafile, 1)
+    reconst_using_model(datafile, out1file, dim, modeltype, modelwidth,
+                        pixelsize=pixelsize, uvmax=uvmax1)
+    with fits.open(out1file) as hdulist:
+        imagehdu = makesf(hdulist[0], fwhm, threshold)
+    reconst_using_image(datafile, get_outputfile(datafile, 2), dim, pixelsize,
+                        imagehdu)
--- tests/test_runbsmem.py
+++ tests/test_runbsmem.py
@@ -6,7 +6,7 @@ try:
 except (OSError, CalledProcessError):
     HAVE_BSMEM = False
 
-from oirunner.runbsmem import run_grey_basic
+from oirunner.runbsmem import run_grey_basic, run_grey_2step
 
 
 class RunBsmemTestCase(unittest.TestCase):
@@ -19,3 +19,8 @@ class RunBsmemTestCase(unittest.TestCase):
         """Test basic grey reconstruction"""
         run_grey_basic(self.datafile)
         run_grey_basic(self.datafile, pixelsize=0.2)
+
+    @unittest.skipUnless(HAVE_BSMEM, "requires bsmem")
+    def test_grey_2step(self):
+        """Test two-step grey reconstruction"""
+        run_grey_2step(self.datafile, pixelsize=0.15)
