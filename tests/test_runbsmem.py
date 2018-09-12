48bf26fa494a8a1c33f9bd05aaeec3e65679a335
John Young <jsy1001@cam.ac.uk>
2018-09-12 14:31:53 +0100

    Write test outputs to temporary directory

--- tests/test_runbsmem.py
+++ tests/test_runbsmem.py
@@ -1,4 +1,7 @@
 import unittest
+import tempfile
+import os.path
+from shutil import copyfile
 from subprocess import run, CalledProcessError
 try:
     run(['bsmem', '-V'])
@@ -17,12 +20,20 @@ class RunBsmemTestCase(unittest.TestCase):
     @unittest.skipUnless(HAVE_BSMEM, "requires bsmem")
     def test_grey_basic(self):
         """Test basic grey reconstruction"""
-        run_grey_basic(self.datafile)
-        run_grey_basic(self.datafile, pixelsize=0.25)
-        run_grey_basic(self.datafile, uvmax=1.1e8)
-        run_grey_basic(self.datafile, pixelsize=0.25, uvmax=1.1e8)
+        with tempfile.TemporaryDirectory() as dirname:
+            tempdatafile = os.path.join(dirname,
+                                        os.path.basename(self.datafile))
+            copyfile(self.datafile, tempdatafile)
+            run_grey_basic(tempdatafile)
+            run_grey_basic(tempdatafile, pixelsize=0.25)
+            run_grey_basic(tempdatafile, uvmax=1.1e8)
+            run_grey_basic(tempdatafile, pixelsize=0.25, uvmax=1.1e8)
 
     @unittest.skipUnless(HAVE_BSMEM, "requires bsmem")
     def test_grey_2step(self):
         """Test two-step grey reconstruction"""
-        run_grey_2step(self.datafile, pixelsize=0.25)
+        with tempfile.TemporaryDirectory() as dirname:
+            tempdatafile = os.path.join(dirname,
+                                        os.path.basename(self.datafile))
+            copyfile(self.datafile, tempdatafile)
+            run_grey_2step(tempdatafile, 0.25)
