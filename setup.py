6cce492e509f45d0563732276d95256e1353478e
John Young <jsy1001@cam.ac.uk>
2022-03-24 13:20:41 +0000

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -6,47 +6,33 @@ import os.path
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the README file
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 
 setup(
-
-    name='oirunner',
-
+    name="oirunner",
     use_scm_version=True,
-
-    setup_requires=['setuptools_scm'],
-
-    description='Tool to reconstruct images from optint data using BSMEM',
-
+    setup_requires=["setuptools_scm"],
+    description="Tool to reconstruct images from optint data using BSMEM",
     long_description=long_description,
-
-    long_description_content_type='text/markdown',
-
-    url='https://github.com/jsy1001/oirunner',
-
-    author='John Young',
-
-    author_email='jsy1001@cam.ac.uk',
-
+    long_description_content_type="text/markdown",
+    url="https://github.com/jsy1001/oirunner",
+    author="John Young",
+    author_email="jsy1001@cam.ac.uk",
     classifiers=[
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-        'Programming Language :: Python :: 3',
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python :: 3",
     ],
-
-    packages=find_packages(exclude=['docs', 'tests']),
-
-    install_requires=['numpy', 'astropy>=1.3', 'scipy<1.4'],
-
+    packages=find_packages(exclude=["docs", "tests"]),
+    install_requires=["numpy", "astropy>=1.3", "scipy<1.4"],
     entry_points={
-        'console_scripts': [
-            'makesf=oirunner.makesf.__main__:main',
+        "console_scripts": [
+            "makesf=oirunner.makesf.__main__:main",
         ],
     },
-
     project_urls={
-        'Bug Reports': 'https://github.com/jsy1001/oirunner/issues',
+        "Bug Reports": "https://github.com/jsy1001/oirunner/issues",
     },
 )
