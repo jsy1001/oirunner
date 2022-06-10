c4d2f20b020d6108f8fab80486d61738fe91632c
John Young <jsy1001@cam.ac.uk>
2022-06-10 15:00:55 +0100

    Add support for Python 3.9
    Update requirements after removing version constraints for astropy and scipy

--- setup.py
+++ setup.py
@@ -24,9 +24,12 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     packages=find_packages(exclude=["docs", "tests"]),
-    install_requires=["numpy", "astropy>=1.3", "scipy<1.4"],
+    install_requires=["numpy", "astropy", "scipy"],
     entry_points={
         "console_scripts": [
             "makesf=oirunner.makesf.__main__:main",
